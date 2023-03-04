e4c23b22d005076bd5956652ffdfe12b0b7e953c
Matthew Wilber <mwilber@gmail.com>
2023-03-03 21:06:50 -0500

    Refactor and cleanup.

--- main.js
+++ main.js
@@ -16,24 +16,39 @@ const Init = () => {
 	buttons = [btnSpeak, btnGpt, btnTest];
 
 	recog = new webkitSpeechRecognition();
-	utterance = new SpeechSynthesisUtterance("");
+	utterance = new SpeechSynthesisUtterance('');
 
 	delay = 50;
 	scale = 8;
 	timer = null;
 
-	throbber.dataset.state = "waiting";
+	ChangeState('waiting');
+
+	btnSpeak.addEventListener('click', Listen);
+	btnTest.addEventListener('click', GetHelp);
+	btnCancel.addEventListener('click', Reset);
+	btnGpt.addEventListener('click', () => QueryAndSpeak('Make up a random sentence.'));
 
 	InitSpeechEvents();
-	InitUiEvents();
 };
 
 const InitSpeechEvents = () => {
 	recog.onresult = (event) => {
-		console.log(event.results[0][0].transcript);
-		let query = event.results[0][0].transcript;
-		//SayIt(response);
-		QueryGPT(query).then((response) => SayIt(response));
+		if(
+			!event ||
+			!event.results ||
+			!event.results.length ||
+			!event.results[0] ||
+			!event.results[0].length ||
+			!event.results[0][0].transcript
+		) {
+			SayIt("I was not able to hear you. Please try again.");
+			Reset();
+		} else {
+			let query = event.results[0][0].transcript;
+			console.log(query);
+			QueryAndSpeak(query);
+		}
 	};
 	
 	utterance.onboundary = (e) => {
@@ -43,9 +58,7 @@ const InitSpeechEvents = () => {
 		const spokenWord = e.utterance.text.substring(e.charIndex, e.charIndex+e.charLength);
 		const nextChar = e.utterance.text.substring(e.charIndex+e.charLength, e.charIndex+e.charLength+1)
 		const charsRemaining = e.utterance.text.length - (e.charIndex+1);
-		//let throbberSize = 10 * charLength;
 	
-		
 		if(/[,.?]/.test(spokenWord)) {
 			throbber.style.transform = '';
 		} else {
@@ -57,94 +70,53 @@ const InitSpeechEvents = () => {
 				throbber.style.transform = 'scale(1)';
 			}, spokenWord.length * delay);
 		}
-	
-		//if (/[,.?\-]/.test(rspace))
-	
-		// throbber.style.transform = 'scale(0)';
-	
-		// if(!/[,.?]/.test(nextChar) && charsRemaining > 0) 
-		//  	setTimeout(() => throbber.style.transform = 'scale(10)', 50);
 	};
 	
-	utterance.addEventListener('start', () => {
-		DisableButtons();
-		console.log('utteranceStart');
-		//throbber.style.transform = 'scale(5)';
-	});
+	utterance.addEventListener('start', console.log('utteranceStart'));
 	utterance.addEventListener('end', () => {
 		console.log('utteranceEnd');
-		throbber.style.animation = "";
-		EnableButtons();
-		//throbber.style.transform = 'scale(3)';
-		
+		Reset();
 	});
 };
 
-const InitUiEvents = () => {
-	btnSpeak.addEventListener('click', () => {
-		DisableButtons();
-		//SayIt('This button has not been programmed to do anything... yet.');
-		throbber.dataset.state = "listening";
-		recog.start();
-	});
-	
-	btnTest.addEventListener('click', () => {
-		DisableButtons();
-		//SayIt('this is a much longer test. there is one long unbroken sentence that keeps moving from topic to topic it is quite hypnotic.');
-		SayIt(`
-			Hello there. I'm GPT 9000, a voice interface for the Chat GPT natural language model by Open A I. To interact with me, press the microphone button to the left and say something. Or, press the dice button on the right and I'll say something random.
-		`);
-	});
-	
-	btnCancel.addEventListener('click', () => {
-		speechSynthesis.cancel();
-		//throbber.style.transform = 'scale(1)';
-		EnableButtons();
-	});
-	
-	btnGpt.addEventListener('click', () => {
-		DisableButtons();
-		QueryGPT("Make up a random sentence.").then((response) => SayIt(response));
-	});
+const GetHelp = () => {
+	SayIt(`
+		Hello there. 
+		I am G P T 9000, a voice interface for the Chat G P T natural language model by Open A I. 
+		To interact with me, press the microphone button to the left and say something. 
+		Or, press the dice button on the right and I'll say something random.
+	`);
 };
 
-
-
-//const voice = new SpeechSynthesisVoice();
-//voice.default = true;
-//voice.lang = "en-US";
-//voice.localService = true;
-//voice.name = "Microsoft David - English (United States)";
-//voice.voiceURI = "Microsoft David - English (United States)";
-
-//utterance.voice = voice;
-
-const DisableButtons = () => {
-	buttons.forEach(btn => btn.setAttribute('disabled', 'true'));
-	btnCancel.removeAttribute('disabled')
+const Listen = () => {
+	ChangeState('listening');
+	recog.start()
 };
 
-const EnableButtons = () => {
-	throbber.dataset.state = "waiting";
-	buttons.forEach(btn => btn.removeAttribute('disabled'));
-	btnCancel.setAttribute('disabled', 'true');
+const Reset = () => {
+	ChangeState('waiting');
+	throbber.style.animation = '';
+	speechSynthesis.cancel();
+	recog.stop();
+};
+
+const QueryAndSpeak = (query) => {
+	QueryGPT(query)
+		.then(
+			(response) => SayIt(response)
+		);
 };
 
 const QueryGPT = (query) => {
-	throbber.dataset.state = "sending";
-	let options = {
-		body: "query=" + query + "",
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-		method: "post",
-	};
+	ChangeState('sending', query);
 
-	gtag('event', 'GPT Query', {
-		'event_category': 'query',
-		'event_label': query,
-		'value': 1
-	});
+	let options = {
+		body: 'query=' + query,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		method: 'post',
+	};
 
 	return fetch('https://proxygpt.greenzeta.com', options)
 		.then(response => response.json())
@@ -152,14 +124,47 @@ const QueryGPT = (query) => {
 };
 
 const SayIt = (txtScript) => {
-	gtag('event', 'Speech API', {
-		'event_category': 'speak',
-		'event_label': txtScript.substring(0, 10),
-		'value': 1
-	});
+	ChangeState('speaking', txtScript);
+
 	utterance.text = txtScript;
-	throbber.dataset.state = "speaking";
 	speechSynthesis.speak(utterance);
+};
+
+const ChangeState = (newState, value) => {
+	const validStates = ['listening', 'speaking','sending'];
+	if(validStates.includes(newState)) {
+		throbber.dataset.state = newState;
+		buttons.forEach(btn => btn.setAttribute('disabled', 'true'));
+		btnCancel.removeAttribute('disabled');
+	} else {
+		throbber.dataset.state = 'waiting';
+		buttons.forEach(btn => btn.removeAttribute('disabled'));
+		btnCancel.setAttribute('disabled', 'true');
+	}
+	AnalyticsEvent(newState, value);
+};
+
+const AnalyticsEvent = (type, value) => {
+	switch(type) {
+		case 'listening':
+			break;
+		case 'speaking':
+			gtag('event', 'Speech API', {
+				'event_category': 'speak',
+				'event_label': value.substring(0, 10),
+				'value': 1
+			});
+			break;
+		case 'sending':
+			gtag('event', 'GPT Query', {
+				'event_category': 'query',
+				'event_label': value.substring(0, 10),
+				'value': 1
+			});
+			break;
+		default:
+			break;
+	}
 };
 
 window[ addEventListener ? 'addEventListener' : 'attachEvent' ]
